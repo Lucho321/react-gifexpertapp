43b201ba4c94a8a7ec7be9293c5ad0a57bd5d670
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2022-03-16 17:57:40 -0600

    updates

---
+++ docs/precache-manifest.2bd86e2cdb3771b3fa19a7325d102638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0a311423be8f122ad7518c7d08ac69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eceee6b90b0b0aabbc8",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eceee6b90b0b0aabbc8",
+    "url": "/static/js/main.19d6fa05.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
