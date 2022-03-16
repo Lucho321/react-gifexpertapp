43b010705e2be511855235687dad126446e24c4f
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2022-03-16 17:54:19 -0600

    updates

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const { data: images, loading } = useFetchGifs(category);
+    const { data:images, loading } = useFetchGifs( category );
 
     return (
         <>
-            <h3 className='animate__animated animate__bounce'> {category}</h3>
+            <h3 className="animate__animated animate__fadeIn"> { category } </h3>
 
-            {loading && <p className='animate__animated animate__flash'>Loading...</p>}
+            { loading && <p className="animate__animated animate__flash">Loading</p> }
 
-            <div className='card-grid'>
+            <div className="card-grid">
+                
                 {
-                    images.map((img) => (
-                        <GifGridItem
-                            key={img.id}
-                            {...img}
+                    images.map( img => (
+                        <GifGridItem 
+                            key={ img.id }
+                            { ...img }
                         />
                     ))
                 }
+            
             </div>
         </>
     )
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,17 +1,21 @@
 
 
-export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=ia22u5O8AWWd65npj0eF0xtk2ZyojZZI`;
-    const resp = await fetch(url);
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    const resp = await fetch( url );
     const { data } = await resp.json();
 
-    const gifs = data.map(img => {
+    const gifs = data.map( img => {
         return {
             id: img.id,
             title: img.title,
             url: img.images?.downsized_medium.url
         }
-    });
-    return(gifs);
+    })
+
+    return gifs;
+
+
 }
