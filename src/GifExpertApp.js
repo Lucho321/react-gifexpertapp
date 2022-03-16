b5429c4559ca783fe4b90b82ea4fbfb471381ef7
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2022-03-16 15:49:02 -0600

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch ']);
+    
+    // const handleAdd = () =>{
+    //     const newCategory = 'Naruto';
+    //     setCategories( [...categories, newCategory] );
+    // }
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+            
+            <AddCategory setCategories={ setCategories } />
+            
+            <ol>
+                {
+                    categories.map( category =>
+                        <GifGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    )
+                }
+            </ol>
+
+
+        </>
+    )
+
+}
